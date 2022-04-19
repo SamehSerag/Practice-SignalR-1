1cc6515722c37c4323bb92365c0b2b4103b124b6
SamehSerag <samehserag1@gmail.com>
2022-04-19 21:48:40 +0200

    first commit

---
+++ RealTimeDisplay/Hubs/EmployeeHub.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.SignalR.Hubs;
+using Microsoft.AspNetCore.SignalR;
+using HubMethodNameAttribute = Microsoft.AspNet.SignalR.Hubs.HubMethodNameAttribute;
+
+namespace RealTimeDisplay.Hubs
+{
+
+    [HubName("employee")]
+    public class EmployeeHub: Hub
+    {
+        //[HubMethodName("sendemployee")]
+        //public void SendEmployee(string name, int salary)
+        //{
+        //    Clients.All.SendAsync("newemp",name, salary);
+        //}
+
+        [HubMethodName("sendemployee")]
+        public void SendEmployee(string fname, string lname,
+            string mobile,string salary, string address)
+        {
+            Clients.All.SendAsync("newemp", fname,  lname,
+                mobile, salary, address);
+        }
+    }
+}
---
+++ SimpleChat/Hubs/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.SignalR.Hubs;
+using Microsoft.AspNetCore.SignalR;
+using HubMethodNameAttribute = Microsoft.AspNet.SignalR.Hubs.HubMethodNameAttribute;
+
+namespace SimpleChat.Hubs
+{
+
+    [HubName("chathub")]
+    public class ChatHub:Hub
+    {
+
+        [HubMethodName("sendmassage")]
+        public void SendMassage(string name, string msg)
+        {
+
+            Clients.All.SendAsync("sendmsg", name, msg);
+        }
+
+    }
+}
